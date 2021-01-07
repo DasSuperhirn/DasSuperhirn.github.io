df520741a9ab14771f482f1705dcc04f14599962
DasSuperhirn <31389735+DasSuperhirn@users.noreply.github.com>
2021-01-07 13:26:17 +0100

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const WebSocket = require("ws");
-
-var socket = new WebSocket("wss://dassuperhirn.github.io", "protocolOne");
-
-socket.onopen = function(event){
-
-    socket.send("haha u mum gay");
-};
